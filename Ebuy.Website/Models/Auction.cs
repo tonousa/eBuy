3127cd11b1414aea9ea291d69e813cb91c3aae21
tonousa <tonousa@hotmail.com>
2016-06-24 16:11:06 -0400

    1

---
+++ Ebuy.Website/Models/Auction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ebuy.Website.Models
+{
+    public class Auction
+    {
+        public long Id { get; set; }
+
+        [Required, StringLength(50, ErrorMessage = "Title no longer of 50 chars")]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Range(1, 10000, ErrorMessage="Starting price must be at least 1")]
+        public decimal StartPrice { get; set; }
+
+        public decimal CurrentPrice { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        [Range(typeof(DateTime), "1/1/2012", "12/31/9999")]
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ Ebuy.Website/Models/EBuyDataContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace Ebuy.Website.Models
+{
+    public class EBuyDataContext : DbContext
+    {
+        public DbSet<Auction> Auctions { get; set; }
+    }
+}
